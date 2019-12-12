29d48b128e87c647e31a166f9b61973d5b417fd5
Mark <markhinschberger@gmail.com>
2019-12-12 21:37:10 +0100

    webiste v2 baby

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,32 +1,15 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '@common/Layout';
+import { Container } from '@components/global';
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = () => (
+  <Layout>
+    <Container>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
+  </Layout>
+);
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
-}
-
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default NotFoundPage;
