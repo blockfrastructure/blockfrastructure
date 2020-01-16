368d4a2ca22fcdacd8cf16ff853cb60d72ee2517
Mark <markhinschberger@gmail.com>
2020-01-16 10:10:52 +0100

    try gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,29 +5,15 @@ const path = require('path');
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       // The property ID; the tracking code won't be generated without it
       trackingId: "UA-153533173-1",
-      head: true,
-      // Setting this parameter is optional
+      head: false,
+      // enable ip anonymization
       anonymize: true,
-      // Setting this parameter is also optional
-      // respectDNT: true,
-      // // Avoids sending pageview hits from custom paths
-      // exclude: ["/preview/**", "/do-not-track/me/too/"],
-      // // Delays sending pageview hits on route update (in milliseconds)
-      // pageTransitionDelay: 0,
-      // Enables Google Optimize using your container Id
-      // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-      // // Enables Google Optimize Experiment ID
-      // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-      // // Set Variation ID. 0 for original 1,2,3....
-      // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-      // Any additional optional fields
-      // sampleRate: 5,
-      // siteSpeedSampleRate: 10,
-      // cookieDomain: "blockfrastructure.com",
     },
+    `gatsby-plugin-react-helmet`,
+
     {
       resolve: `gatsby-plugin-svgr`,
       options: {
@@ -38,7 +24,6 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-react-helmet`,
 
     `gatsby-plugin-styled-components`,
     {
