26ac17dc7e09499e3c1cfed487c55f360be68dcf
Mark <markhinschberger@gmail.com>
2020-01-19 12:42:56 +0100

    remove gtag for migration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,32 @@
 const path = require('path');
-// const { GOOGLE_ANALYTICS_TRACKING_ID } = process.env;
-// require('dotenv').config();
 
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-plugin-gtag`,
-      // The property ID; the tracking code won't be generated without it
-      trackingId: "UA-153533173-1",
-      head: false,
-      // enable ip anonymization
-      anonymize: true,
-    },
     `gatsby-plugin-react-helmet`,
-
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      // The property ID; the tracking code won't be generated without it
+      trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "NO GA",
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+      // Avoids sending pageview hits from custom paths
+      exclude: ["/preview/**", "/do-not-track/me/too/"],
+      // Delays sending pageview hits on route update (in milliseconds)
+      pageTransitionDelay: 0,
+      // Enables Google Optimize using your container Id
+      // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+      // // Enables Google Optimize Experiment ID
+      // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+      // // Set Variation ID. 0 for original 1,2,3....
+      // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+      // Any additional optional fields
+      sampleRate: 5,
+      siteSpeedSampleRate: 10,
+      cookieDomain: "blockfrastructure.com",
+    },
     {
       resolve: `gatsby-plugin-svgr`,
       options: {
@@ -24,7 +37,6 @@ module.exports = {
         },
       },
     },
-
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
